6ed8b1f0d29fb63f887dea7d18208ad98c9f09bf
juliensilance@hotmail.com <juliensilance@MacBook-Pro-de-Julien.local>
2023-04-22 13:04:58 +0200

    ajustement

--- srcs/ft_key_event.c
+++ srcs/ft_key_event.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_key_event.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jusilanc <jusilanc@s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 00:02:32 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/04/21 16:53:25 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/04/22 13:04:33 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,13 @@ int	mouse_event(int keycode, int x, int y, t_mdata *dat)
 {
 	(void)y;
 	if (keycode == 4)
-		dat->zoom /= 1.2;
+		dat->zoom /= 1.1;
 	else if (keycode == 5)
-		dat->zoom *= 1.2;
-	dat->mos_x += ft_fmap(x, dat->size.x, -dat->zoom, dat->zoom);
-	dat->mos_y += ft_fmap(y, dat->size.y, -dat->zoom, dat->zoom);
+		dat->zoom *= 1.1;
+	if (keycode == 4 || keycode == 5)
+	{
+		dat->mos_x += ft_fmap(x, dat->size.x, -dat->zoom, dat->zoom);
+		dat->mos_y += ft_fmap(y, dat->size.y, -dat->zoom, dat->zoom);
+	}
 	return (38);
 }
